8c0024e462e1f4912d0d8f9292ab898bf8bdeaa6
RamishMunawar <128965516+RamishMunawar@users.noreply.github.com>
2024-02-24 17:26:12 +0500

    dashboard

---
+++ js/javaScript.js
@@ -0,0 +1,15 @@
+// var a;
+
+// function pass(){
+//     if(a==1){
+//         document.getElementById('password').type='password';
+//         document.getElementById('imge_1').src='download-removebg-preview';
+//         a=0;
+
+//     }
+//     else{
+//         document.getElementById('password').type='text';
+//         document.getElementById('imge_1').src='download.png';
+//         a=1;
+//     }
+// }
